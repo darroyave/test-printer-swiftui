3c3a7719fd23e41e19776841fbf54c3210d02b81
Dannover Arroyave M <dannover@gmail.com>
2025-06-25 16:24:48 -0500

    Update BluetoothDetailView.swift

--- PrintTest/BluetoothDetailView.swift
+++ PrintTest/BluetoothDetailView.swift
@@ -1,74 +1,20 @@
-//
-//  BluetoothManager.swift
-//  PrintTest
-//
-//  Created by Dannover Arroyave on 25/06/25.
-//
-
-import Foundation
+import SwiftUI
 import CoreBluetooth
 
-class BluetoothManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralDelegate {
-    var discoveredDevices: [CBPeripheral] = []
-    var selectedDevice: CBPeripheral?
+struct BluetoothDetailView: View {
+    let peripheral: CBPeripheral
     
-    @Published var bluetoothState: CBManagerState = .unknown
-    @Published var isScanning: Bool = false
-    
-    private var centralManager: CBCentralManager!
-
-    override init() {
-        super.init()
-        centralManager = CBCentralManager(delegate: self, queue: .main)
-    }
-
-    func startScan() {
-        discoveredDevices.removeAll()
-        centralManager.scanForPeripherals(withServices: nil, options: nil)
-        isScanning = true
-    }
-    
-    func stopScan() {
-        centralManager.stopScan()
-        isScanning = false
-    }
-    
-    func connect(to peripheral: CBPeripheral) {
-        selectedDevice = peripheral
-        stopScan()
-        centralManager.connect(peripheral, options: nil)
-    }
-    
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        bluetoothState = central.state
+    var body: some View {
         
-        if central.state == .poweredOn {
-            startScan()
-        } else {
-            stopScan()
-            print("Bluetooth is off or not available")
+        VStack(alignment: .leading, spacing: 10) {
+            
+            Text("Device Name: \(peripheral.name ?? "Unknown")")
+                .font(.headline)
+            
+            Text("Identifier: \(peripheral.identifier.uuidString)")
+            
         }
+        .padding()
+        .navigationTitle("Device Details")
     }
-
-    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        if !discoveredDevices.contains(where: { $0.identifier == peripheral.identifier }) {
-            discoveredDevices.append(peripheral)
-        }
-    }
-    
-    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        print("Connected to \(peripheral.name ?? "Unknown device")")
-        peripheral.delegate = self
-        peripheral.discoverServices(nil)
-    }
-    
-    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: (any Error)?) {
-        print("Failed to connect to \(error?.localizedDescription ?? "Unknown error")")
-    }
-    
-    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: (any Error)?) {
-        print("Disconnected from \(peripheral.name ?? "Unknown device")")
-        selectedDevice = nil
-    }
-    
 }
