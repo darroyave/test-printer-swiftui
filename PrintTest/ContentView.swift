dcf33b8410a33419741228751329c07b3e66dbc0
Dannover Arroyave M <dannover@gmail.com>
2025-06-25 16:21:59 -0500

    Update ContentView.swift

--- PrintTest/ContentView.swift
+++ PrintTest/ContentView.swift
@@ -9,21 +9,44 @@ import SwiftUI
 import CoreBluetooth
 
 struct ContentView: View {
-    private var bluetoothManager = BluetoothManager()
+    
+    @StateObject private var bluetoothManager = BluetoothManager()
     @State private var selectedDevice: CBPeripheral?
 
     var body: some View {
+        
         NavigationView {
-            List(bluetoothManager.discoveredDevices, id: \ .identifier) { device in
-                NavigationLink(destination: BluetoothDetailView(peripheral: device)) {
-                    Text(device.name ?? "Unknown Device")
-                        .onTapGesture {
-                            selectedDevice = device
-                            bluetoothManager.connect(to: device)
+            
+            VStack {
+                
+                if bluetoothManager.bluetoothState == .poweredOff {
+                    
+                    List(bluetoothManager.discoveredDevices, id: \ .identifier) { device in
+                        NavigationLink(destination: BluetoothDetailView(peripheral: device)) {
+                            Text(device.name ?? "Unknown Device")
+                                .onTapGesture {
+                                    selectedDevice = device
+                                    bluetoothManager.connect(to: device)
+                                }
                         }
+                    }
+                    
+                    if !bluetoothManager.isScanning {
+                        Button("Start Scanning") {
+                            bluetoothManager.startScan()
+                        }
+                    } else {
+                        Button("Stop Scanning") {
+                            bluetoothManager.stopScan()
+                        }
+                    }
+                    
+                } else {
+                    Text("Bluetooth is not powered on.")
                 }
             }
             .navigationTitle("Bluetooth Scanner")
+            
         }
     }
 }
